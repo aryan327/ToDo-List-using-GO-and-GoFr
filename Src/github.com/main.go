6b72a53e1a00d79b7f9aea91e79486945464a2ad
Aryan Pathak <82660401+aryan327@users.noreply.github.com>
2023-12-16 18:03:17 +0530

    Added routes for update car and Delete Car

--- Src/github.com/main.go
+++ Src/github.com/main.go
@@ -16,13 +16,22 @@ func GetCars(ctx *gofr.Context) {
 
 }
 
+func UpdateCar(ctx *gofr.Context) {
+
+}
+
+func DeleteCar(ctx *gofr.Context) {
+
+}
+
 func main() {
 	initDB()
 
 	app := gofr.New()
 	app.POST("/cars", CreateCar)
 	app.GET("/cars", GetCars)
-
+	app.PUT("/cars", UpdateCar)
+	app.DELETE("/cars", DeleteCar)
 
 	app.Run(":8080")
 }
