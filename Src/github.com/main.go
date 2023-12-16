f6e762e3fdca799ee0beb02732d51e21b2a7e24a
Aryan Pathak <82660401+aryan327@users.noreply.github.com>
2023-12-16 18:54:35 +0530

    Initialize database connection & logic for creating a car

--- Src/github.com/main.go
+++ Src/github.com/main.go
@@ -5,10 +5,21 @@ import (
 )
 
 func initDB() {
-
+dsn := "user:password@tcp(localhost:8080)/dbname?parseTime=true"
+var err error
+db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+if err != nil {
+	panic("Failed to connect to the database: " + err.Error())
 }
 
 func CreateCar(ctx *gofr.Context) {
+	var newCar Car
+	err := ctx.JSONBody(&newCar)
+	if err != nil {
+		// Handle the error, e.g., invalid JSON
+		ctx.JSON(400, map[string]interface{}{"error": "Invalid JSON"})
+		return
+	}
 
 }
 
